57af4c71c219aec389521ac2e3dfdb0ca2391d46
Marius <avramescu.marius@gmail.com>
2018-04-08 23:25:54 +0300

    tests ok

--- VendingM/Program.cs
+++ VendingM/Program.cs
@@ -10,18 +10,21 @@ namespace VendingM
     {
         static void Main(string[] args)
         {
-            
-
             //task
             //A vending machine sells items for various prices and can give change. At the start of the day it is loaded with a certain number of coins of various denominations e.g. 100 x 1p, 50 x 5p, 50 x 10p etc. 
             //When an item is requested a certain number of coins are provided. 
             //Write code that models the vending machine and calculates the change to be given when an item is purchased (e.g. 2 x 20p used to purchase an item costing 25p might return 1 x 10p and 1 x 5p).
-            
+
+            //about coding
+            //all done in one file (for easy archiving and explanations)
+            //input is done through txt files (in the same folder as the executable) chosen by the user from the console.
+            //two important classes - vendingMachine and userInteraction
+            //the interactions will succeed if items or money are available. if not, user is notified by messages
             //the use of 'ref' was just for debugging the program faster and to show the flow of operations easier in the lines below
 
 
             Console.WindowHeight = 60; //increase height of the console 
-            Console.WindowWidth= 140; //increase height of the console 
+            Console.WindowWidth = 140; //increase height of the console 
             Program p = new Program();
             Console.WriteLine("Welcome to Vending Machine 1.0!");
             Console.WriteLine();
@@ -43,7 +46,7 @@ namespace VendingM
             p.display(VM.money);
             Console.WriteLine();
 
-            Console.WriteLine("Write 'p [fileName]' to purchase items. e.g. p UI_interact.txt");
+            Console.WriteLine("Write 'p [fileName]' to purchase items. e.g.   p UI_interact.txt   or   p UI_interact2.txt");
             Console.WriteLine("Write 'q' to leave VM");
             Console.WriteLine();
 
@@ -64,11 +67,14 @@ namespace VendingM
                     try
                     {
                         fileName = userConsole.Split(' ')[1];
+                        //try the path
+                        StreamReader sr = new StreamReader(fileName);
+                        sr.Dispose();
                     }
                     catch
                     {
                         Console.WriteLine("We do not have that product. Input error");
-                        break;
+                        continue;
                     }
 
                     counter += 1;
@@ -95,6 +101,11 @@ namespace VendingM
                     //display change
                     Console.Write("Change received: ");
                     p.display(UI.UI_changeGiven);
+                    if (UI.UI_changeGiven.Count == 0)
+                    {
+                        //cannot give change
+                        Console.WriteLine(VM.message);
+                    }
 
                     //display VM money
                     Console.Write("VM money after interaction: ");
@@ -103,6 +114,8 @@ namespace VendingM
                     //display VM items
                     Console.Write("VM items after interaction: ");
                     p.display(VM.items);
+
+                    Console.WriteLine();
                 }
                 else
                 {
@@ -138,7 +151,7 @@ namespace VendingM
         {
             for (int i = 0; i < e.Count; i++)
             {
-                Console.Write(e[i].name +" "+e[i].quantity + "x" + e[i].value + "p");
+                Console.Write(e[i].name + " " + e[i].quantity + "x" + e[i].value + "p");
                 if (i == e.Count - 1)
                 {
                     Console.Write("\n");
@@ -149,7 +162,7 @@ namespace VendingM
                 }
             }
         }
- 
+
     }
 
     public class vendingMachine : entity
@@ -157,7 +170,9 @@ namespace VendingM
         public List<item> items { get; set; }
         public List<money> money { get; set; }
 
-        //for any timeframe desired - for now we leave it as an all-time evidence
+        public string message { get; set; }
+
+        //for any timeframe desired - for now we leave it as an all-time evidence -TODO
         public int VM_itemsSoldCount { get; set; }
         public int VM_moneyGainedCount { get; set; }
         public List<item> VM_itemsSold { get; set; }
@@ -177,18 +192,15 @@ namespace VendingM
             return price;
         }
 
-        
+
         public void giveChange(ref vendingMachine VM, ref userInteraction UI)
         {
-            List<money> result = new List<money>();
-            
             //see if items are available in stock
-            //this should not happen! (you should not ask for an item that is not in VM
-
-            bool stock=true;
+            //this should not happen! (you should not ask for an item that is not in VM)
+            bool stock = true;
             for (int i = 0; i < UI.UI_itemsRequested.Count; i++)
             {
-                
+
                 for (int j = 0; j < VM.items.Count; j++)
                 {
                     if (UI.UI_itemsRequested[i].id.Equals(VM.items[j].id))
@@ -203,7 +215,8 @@ namespace VendingM
             }
             if (!stock)
             {
-                //todo message for user
+                //message for user - what products are not avaiable? maybe at VM 2.0
+                VM.message = "One of the products is not on stock! Interaction cancelled. Please take your money back";
                 return;
             }
 
@@ -217,11 +230,11 @@ namespace VendingM
                     cost += price;
                 else
                 {
-                    //treat error TODO
-                    //error in file UI - ID not found -error in file, or the item with that ID is no longer in stock
-
-                    //we have to remove the item form the list of items of the interaction
-                    //compose message for user
+                    //price =-1;
+                    //error in file UI - ID not found -error in file, 
+                    //message for user
+                    VM.message = "One of the products is not in the menu! Interaction cancelled. Please take your money back";
+                    return;
                 }
             }
 
@@ -235,11 +248,15 @@ namespace VendingM
 
             if (change < 0)
             {
-                //more money for the products please
+                //insert more money for the products please - this should be the proper response but since we get data from a txt file, we have to do another interation to read a new file
+                VM.message = "You did not insert enough money. Interaction cancelled. Please take your money back";
+                return;
             }
             else if (change == 0)
             {
                 //end transaction with a message
+                VM.message = "Exact sum provided. No change needed.";
+                //return;
             }
             else
             {
@@ -250,14 +267,14 @@ namespace VendingM
                 while (changePossible)
                 {
                     int max = 0;
-                    int indexMax=0;
+                    int indexMax = 0;
                     for (int i = 0; i < VM.money.Count; i++)
                     {
                         //find max - but smaller than change
                         if ((VM.money[i].value > max) && (VM.money[i].quantity > 0) && (VM.money[i].value <= change))
                         {
-                            max = VM.money[i].value; 
-                            indexMax=i;
+                            max = VM.money[i].value;
+                            indexMax = i;
                         }
                     }
                     change -= max;
@@ -291,80 +308,82 @@ namespace VendingM
                     }
 
                     //while loop condition
-                    if ((max == 0) || (change ==0))
+                    if ((max == 0) || (change == 0))
                     {
                         //either we completed all the change or we cannot give change so we abort
                         changePossible = false;
                     }
                 }
 
-                if (change == 0)
+            }
+            if (change == 0)
+            {
+                //mission complete 
+                //update items in VM  
+                //update givenitems in UI 
+
+                for (int i = 0; i < UI.UI_itemsRequested.Count; i++)
                 {
-                    //mission complete 
-                    //update items in VM  -TODO
-                    //update givenitems in UI -TODO
-
-                    for (int i = 0; i < UI.UI_itemsRequested.Count; i++)
+                    for (int j = 0; j < VM.items.Count; j++)
                     {
-                        for (int j = 0; j < VM.items.Count; j++)
+                        if (UI.UI_itemsRequested[i].id.Equals(VM.items[j].id))
                         {
-                            if (UI.UI_itemsRequested[i].id.Equals(VM.items[j].id))
-                            {
-                                VM.items[j].quantity -= UI.UI_itemsRequested[i].quantity;
-                            }
+                            VM.items[j].quantity -= UI.UI_itemsRequested[i].quantity;
                         }
                     }
-
-                    //update money in VM (with money given by the user) - only if the transaction is finalized (the change could be given) - we do not simulate the money going in VM and exiting VM if the change cannot be given
-
-                    for (int j = 0; j < UI.UI_moneyUsed.Count; j++)
-                    {
-
-                        money m = UI.UI_moneyUsed[j];
-
-                        bool alreadyExists = false;
-                        for (int i = 0; i < VM.money.Count; i++)
-                        {
-                            if (m.value.Equals(VM.money[i].value))
-                            {
-                                alreadyExists = true;
-                                VM.money[i].quantity += m.quantity;
-                                break;
-                            }
-                        }
-
-                        if (!alreadyExists)
-                        {
-                            UI.UI_changeGiven.Add(m);
-                        }
-                    }
-
- 
                 }
-                else
+
+                //update money in VM (with money given by the user) - only if the transaction is finalized (the change could be given) - we do not simulate the money going in VM and exiting VM if the change cannot be given
+
+                for (int j = 0; j < UI.UI_moneyUsed.Count; j++)
                 {
-                    //no change to give!!!
 
-                    //update VM money using UI.changeGiven
-                    //empty UI.changeGiven
+                    money m = UI.UI_moneyUsed[j];
 
-                    for (int i = 0; i < UI.UI_changeGiven.Count; i++)
+                    bool alreadyExists = false;
+                    for (int i = 0; i < VM.money.Count; i++)
                     {
-                        for (int j = 0; j < VM.money.Count; j++)
+                        if (m.value.Equals(VM.money[i].value))
                         {
-                            if (UI.UI_changeGiven[i].value.Equals(VM.money[j].value))
-                            {
-                                VM.money[j].quantity += UI.UI_changeGiven[i].quantity;
-                            }
+                            alreadyExists = true;
+                            VM.money[i].quantity += m.quantity;
+                            break;
                         }
                     }
 
-                    UI.UI_changeGiven.Clear();
-
-                    //create message for user -TODO
+                    if (!alreadyExists)
+                    {
+                        VM.money.Add(m);
+                    }
                 }
+
 
             }
+            else
+            {
+                //no change to give!!!
+
+                //update VM money using UI.changeGiven
+                //empty UI.changeGiven
+
+                for (int i = 0; i < UI.UI_changeGiven.Count; i++)
+                {
+                    for (int j = 0; j < VM.money.Count; j++)
+                    {
+                        if (UI.UI_changeGiven[i].value.Equals(VM.money[j].value))
+                        {
+                            VM.money[j].quantity += UI.UI_changeGiven[i].quantity;
+                        }
+                    }
+                }
+
+                UI.UI_changeGiven.Clear();
+
+                //create message for user
+                VM.message = "No change to give! Interaction cancelled. Place take your money back.";
+            }
+
+
 
         }
 
@@ -373,7 +392,7 @@ namespace VendingM
             //input from a text file (an XML would be better)
             //but for now txt is fine, because the user will input interactions in the same format (no visual elements, e.g. buttons)
 
-            
+
 
             string fileName = "VM_fill.txt";
             string[] separators = new string[1];
@@ -381,14 +400,13 @@ namespace VendingM
 
             using (StreamReader sr = new StreamReader(fileName))
             {
-                //while (!sr.EndOfStream)
-                //{
-
+                //read items from file
                 string[] el = sr.ReadLine().Split(separators, StringSplitOptions.None); // StringSplitOptions.None will leave the indexes ok if some data is missing from the file
                 //el[0] = title of item fields: id  name	category	value	quantity
                 //el[1] = item1
                 //el[2] = item2, etc
 
+                //add items to VM
                 for (int i = 1; i < el.Length; i++)
                 {
                     string[] elements = el[i].Split('\t');
@@ -408,14 +426,14 @@ namespace VendingM
 
                     VM.items.Add(z);
                 }
-                
 
+                //read money from file
                 el = sr.ReadLine().Split(separators, StringSplitOptions.None); // StringSplitOptions.None will leave the indexes ok if some data is missing from the file
                 //el[0] = title of money fields: value	quantity
                 //el[1] = money1
                 //el[2] = money2, etc                   
 
-
+                //add money to VM
                 for (int i = 1; i < el.Length; i++)
                 {
                     string[] elements = el[i].Split('\t');
@@ -430,8 +448,6 @@ namespace VendingM
                     VM.money.Add(z);
                 }
 
-
-                //}
                 sr.Close();
             }
 
@@ -444,28 +460,25 @@ namespace VendingM
         public List<item> UI_itemsRequested { get; set; }
         public List<money> UI_moneyUsed { get; set; }
 
-        public List<money> UI_moneyRequested { get; set; }
-        public List<item> UI_itemsGiven { get; set; } //in case the machine runs out of change - gives less items if no change available (even 0 items)  (TODO - discussion)
         public List<money> UI_changeGiven { get; set; }
 
         public void interact(ref userInteraction UI, string fileName)
         {
-            //input from a text file for now
+            //input from a text file
 
-          //string fileName = "UI_interact.txt";
+            //string fileName = "UI_interact.txt";
             string[] separators = new string[1];
             separators[0] = ", ";
 
             using (StreamReader sr = new StreamReader(fileName))
             {
-                //while (!sr.EndOfStream)
-                //{
-
+                //read items from file
                 string[] el = sr.ReadLine().Split(separators, StringSplitOptions.None); // StringSplitOptions.None will leave the indexes ok if some data is missing from the file
                 //el[0] = title of item fields: id
                 //el[1] = item1
                 //el[2] = item2, etc
 
+                //add items to requested list
                 for (int i = 1; i < el.Length; i++)
                 {
                     string[] elements = el[i].Split('\t');
@@ -481,13 +494,13 @@ namespace VendingM
                     UI.UI_itemsRequested.Add(z);
                 }
 
-
+                //read money from file
                 el = sr.ReadLine().Split(separators, StringSplitOptions.None); // StringSplitOptions.None will leave the indexes ok if some data is missing from the file
                 //el[0] = title of money fields: value	quantity
                 //el[1] = money1
                 //el[2] = money2, etc                   
 
-
+                //add money to list
                 for (int i = 1; i < el.Length; i++)
                 {
                     string[] elements = el[i].Split('\t');
@@ -502,8 +515,6 @@ namespace VendingM
                     UI.UI_moneyUsed.Add(z);
                 }
 
-
-                //}
                 sr.Close();
 
             }
@@ -513,8 +524,6 @@ namespace VendingM
     public class money : entity
     {
         //public string type { get; set; } // banknote or coin - if we decide to add type --> the value for banknotes should be multiplied by 100, it is safer to have only one currency - e.g cent or penny (instead of having both cent and dollar)
-
-        
     }
 
     public class item : entity
