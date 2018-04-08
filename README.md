2e27a53b584fd29dfc1cd5b2297a46ddad9376da
Marius <avramescu.marius@gmail.com>
2018-04-09 00:00:33 +0300

    final touch

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # VendingM
 
-visual studio 2010 application
+Visual studio 2010 - console application
+
+A vending machine sells items for various prices and can give change. At the start of the day it is loaded with a certain number of coins of various denominations e.g. 100 x 1p, 50 x 5p, 50 x 10p etc. 
+When an item is requested a certain number of coins are provided. 
+Write code that models the vending machine and calculates the change to be given when an item is purchased (e.g. 2 x 20p used to purchase an item costing 25p might return 1 x 10p and 1 x 5p).
+
+The application - allows any number of items to be purchased. Not just one.
+
+Use of the application: run commands like: p UI_interact.txt.
+The application was tested using the 3 text files provided. You can modify them or create new ones using the same structure as described below. 
+
+Input - from text files
+structure:
+first line in each file is about the items. Elements of the line are separated by commas. First element shows the fields for the item e.g value or quantity. The rest of the elements are the items (the fields are separated by tabs)
+second line in each file is about the money. Elements of the line are separated by commas. First element shows the fields for the money e.g value or quantity. The rest of the elements are the money (the fields are separated by tabs)
+
+Output - change given and status of money and items in the vending machine for every interaction.
--- VendingM/Program.cs
+++ VendingM/Program.cs
@@ -10,17 +10,35 @@ namespace VendingM
     {
         static void Main(string[] args)
         {
-            //task
+
+            //            from README
+            //            Visual studio 2010 - console application
+
             //A vending machine sells items for various prices and can give change. At the start of the day it is loaded with a certain number of coins of various denominations e.g. 100 x 1p, 50 x 5p, 50 x 10p etc. 
             //When an item is requested a certain number of coins are provided. 
             //Write code that models the vending machine and calculates the change to be given when an item is purchased (e.g. 2 x 20p used to purchase an item costing 25p might return 1 x 10p and 1 x 5p).
 
-            //about coding
+            //The application - allows any number of items to be purchased. Not just one.
+
+            //Use of the application: run commands like: p UI_interact.txt.
+            //The application was tested using the 3 text files provided. You can modify them or create new ones using the same structure as described below. 
+
+            //Input - from text files
+            //structure:
+            //first line in each file is about the items. Elements of the line are separated by commas. First element shows the fields for the item e.g value or quantity. The rest of the elements are the items (the fields are separated by tabs)
+            //second line in each file is about the money. Elements of the line are separated by commas. First element shows the fields for the money e.g value or quantity. The rest of the elements are the money (the fields are separated by tabs)
+
+            //Output - change given and status of money and items in the vending machine for every interaction.
+
+
+            //about the coding
             //all done in one file (for easy archiving and explanations)
             //input is done through txt files (in the same folder as the executable) chosen by the user from the console.
-            //two important classes - vendingMachine and userInteraction
+            //two important classes for modelling the vending machine and the users actions - vendingMachine and userInteraction
+            //two classes for objects - items from the vending machine and money
             //the interactions will succeed if items or money are available. if not, user is notified by messages
             //the use of 'ref' was just for debugging the program faster and to show the flow of operations easier in the lines below
+            //hope all cases where covered. The 3 text files worked OK, in any succession in the same session.
 
 
             Console.WindowHeight = 60; //increase height of the console 
@@ -34,6 +52,7 @@ namespace VendingM
             //init VM
             VM.items = new List<item>();
             VM.money = new List<money>();
+            VM.VM_moneyGained = new List<money>();
             //other inits?
 
             //input
@@ -59,6 +78,8 @@ namespace VendingM
                 string userConsole = Console.ReadLine();
                 if (userConsole.Equals("q"))
                 {
+                    Console.Write("Money that came in VM today: ");
+                    p.display(VM.VM_moneyGained);
                     break;
                 }
                 else if (userConsole.Contains('p'))
@@ -173,10 +194,11 @@ namespace VendingM
         public string message { get; set; }
 
         //for any timeframe desired - for now we leave it as an all-time evidence -TODO
-        public int VM_itemsSoldCount { get; set; }
-        public int VM_moneyGainedCount { get; set; }
-        public List<item> VM_itemsSold { get; set; }
-        public List<money> VM_moneyGained { get; set; }
+        public int VM_itemsSoldCount { get; set; } //-TODO
+        public int VM_moneyGainedCount { get; set; }//-TODO
+        public List<item> VM_itemsSold { get; set; }//-TODO
+        public List<money> VM_moneyGained { get; set; } //done    -- this is the money that came in (it does not include what came out)
+        public List<money> VM_moneyLostAsChange { get; set; } //-TODO
 
         public int getPriceOf(int itemID, vendingMachine VM)
         {
@@ -354,6 +376,31 @@ namespace VendingM
                     if (!alreadyExists)
                     {
                         VM.money.Add(m);
+                    }
+                }
+
+
+                //bonus - statistics for VM
+                //do the same for the total gains stored in VM_moneyGained
+                for (int j = 0; j < UI.UI_moneyUsed.Count; j++)
+                {
+
+                    money m = UI.UI_moneyUsed[j];
+
+                    bool alreadyExists = false;
+                    for (int i = 0; i < VM.VM_moneyGained.Count; i++)
+                    {
+                        if (m.value.Equals(VM.VM_moneyGained[i].value))
+                        {
+                            alreadyExists = true;
+                            VM.VM_moneyGained[i].quantity += m.quantity;
+                            break;
+                        }
+                    }
+
+                    if (!alreadyExists)
+                    {
+                        VM.VM_moneyGained.Add(m);
                     }
                 }
 
